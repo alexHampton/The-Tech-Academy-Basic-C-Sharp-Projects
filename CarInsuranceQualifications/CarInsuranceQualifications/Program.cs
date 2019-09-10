71864a8fa17e5ad307152a2ad075612601673f0a
Alex Hampton <ahampton5158@gmail.com>
2019-09-10 16:23:31 +0900

    Add CarInsuranceQualifications exercise

---
+++ CarInsuranceQualifications/CarInsuranceQualifications/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceQualifications
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string sAge = Console.ReadLine();
+            Console.WriteLine("Have you ever had a DUI?");
+            string sDUI = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have?");
+            string sTickets = Console.ReadLine();
+
+            //data type conversions
+            int age = Convert.ToInt32(sAge);
+            bool DUI = Convert.ToBoolean(sDUI);
+            int tickets = Convert.ToInt32(sTickets);
+
+            bool qualified = (age > 15 && !DUI && tickets <= 3);
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(qualified);
+
+            Console.Read();
+
+        }
+    }
+}
