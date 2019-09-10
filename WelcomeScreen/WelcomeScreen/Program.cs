b1cce98bf696e69ececbd929ec8a91002f9cca58
Alex Hampton <ahampton5158@gmail.com>
2019-09-10 11:19:46 +0900

    Add Welcome Screen

---
+++ WelcomeScreen/WelcomeScreen/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace WelcomeScreen
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Acconting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
