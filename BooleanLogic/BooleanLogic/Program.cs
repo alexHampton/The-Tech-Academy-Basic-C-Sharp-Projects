d4aaed7e43de0761bb7b58474049f9144fa44301
Alex Hampton <ahampton5158@gmail.com>
2019-09-10 16:14:28 +0900

    Add BooleanLogic

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // AND OPERATOR
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(false && true);
+            //Console.WriteLine(false && false);
+            //Console.WriteLine();
+
+            // OR OPERATOR
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || true);
+            //Console.WriteLine(false || false);
+            //Console.WriteLine();
+
+            // EQUAL OPERATOR
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+            //Console.WriteLine();
+
+            // NOT EQUAL OPERATOR
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+            //Console.WriteLine();
+
+            // XOR OPERATOR
+            Console.WriteLine(true ^ true);
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(false ^ true);
+            Console.WriteLine(false ^ false);
+
+            Console.Read();
+
+        }
+    }
+}
