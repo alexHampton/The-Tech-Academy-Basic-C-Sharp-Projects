8189293fc30809d299dab83fb8dee49f3a1bf66d
Alex Hampton <ahampton5158@gmail.com>
2019-09-12 08:40:55 +0900

    Add ClassAndMethodExercise3

---
+++ ClassAndMethodexercise3/ClassAndMethodexercise3/MathOps.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodexercise3
+{
+    class MathOps
+    {
+        // Object constructor
+        public MathOps()
+        {
+
+        }
+
+        // Create a method that will take an integer, do a math operation to it
+        // and then return theanswer as an integer.
+        public int numberMath(int num)
+        {
+            return num * num;
+        }
+
+        // Add a second method to the class, with the same name, that will take
+        // in a decimal, do a different math operation to it and then return 
+        // the answer as an integer.
+        public int numberMath(decimal numOne)
+        {
+            return Convert.ToInt32(numOne) + 5;
+        }
+
+        // Add a third method to the class, with the same name, that will take
+        // in a string, convert it to an integer if possible, do a different 
+        // math operation to it and then return the answer as an integer.
+        public int numberMath(string num)
+        {
+            int numInt = Convert.ToInt32(num);
+            return numInt - 5;
+        }
+
+    }
+}
---
+++ ClassAndMethodexercise3/ClassAndMethodexercise3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodexercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the class
+            MathOps objOne = new MathOps();
+            //Call the method, passing in an integer.
+            // Display the result to the screen.
+            Console.WriteLine("Using Method 1, we get: ");
+            Console.WriteLine(objOne.numberMath(456));
+
+
+            // Instantiate the class and call the second method, passing in 
+            // a decimal. 
+            // Display the result to the screen.
+            Console.WriteLine("Using Method 2, we get: ");
+            Console.WriteLine(objOne.numberMath(12.45m));
+
+
+            //Instantiate the class and call the third method, passing in a 
+            // string that equates to an integer.
+            Console.WriteLine("Using Method 3, we get: ");
+            Console.WriteLine(objOne.numberMath("40"));
+
+            Console.Read();
+
+
+        }
+    }
+}
