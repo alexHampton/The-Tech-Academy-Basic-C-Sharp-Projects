f12e2e849b6bc2273d0dcf76d32f589b9d6aeb84
Alex Hampton <ahampton5158@gmail.com>
2019-09-11 14:48:39 +0900

    Add Class and method exercise

---
+++ ClassAndMethodExercise/ClassAndMethodExercise/MathOps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodExercise
+{
+    class MathOps // The Class is MathOps
+    {
+        // This is the Constructor for MathOps. It takes in an integer, then does
+        // 3 different math operations on that integer, and saves the output of those
+        // operations into 3 different variables.
+        public MathOps(int userNum) 
+        {
+            this.AddTen(userNum); // Operation/Method 1.
+            this.Square(userNum); // Operation/Method 2.
+            this.MinusFour(userNum); // Operation/Method 3.
+        }
+
+        // Method 1: Add 10
+        public void AddTen(int n)
+        {
+            addTen = n + 10;
+        }
+
+        // Method 2: Square
+        public void Square(int n)
+        {
+            square = n * n;
+        }
+
+        //Method 3: Subtract 4
+        public void MinusFour(int n)
+        {
+            minusfour = n - 4;
+        }
+        
+        // These are the actual variables where the outputs are saved.
+        public int addTen;
+        public int square;
+        public int minusfour;
+    }
+}
---
+++ ClassAndMethodExercise/ClassAndMethodExercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What number would you like to do math operations on?");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            MathOps firstOne = new MathOps(userInput);
+
+
+            Console.WriteLine(firstOne.addTen);
+            Console.WriteLine(firstOne.square);
+            Console.WriteLine(firstOne.minusfour);
+
+
+            Console.WriteLine("Ok, now input another number, so we can create another object.");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+            MathOps secondOne = new MathOps(userInput1);
+
+            Console.WriteLine(secondOne.addTen);
+            Console.WriteLine(secondOne.square);
+            Console.WriteLine(secondOne.minusfour);
+
+            Console.Read();
+
+
+        }
+        
+    }
+}
