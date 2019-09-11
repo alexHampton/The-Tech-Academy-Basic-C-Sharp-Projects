60723f49489510eeaec47f3c89b6489b549db35c
Alex Hampton <ahampton5158@gmail.com>
2019-09-12 07:56:09 +0900

    Add ClassAndMethodexercise2

---
+++ ClassAndMethodExercise2/ClassAndMethodExercise2/MathOps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodExercise2
+{
+    class MathOps
+    {
+        public MathOps() // MathOps object constructor
+        {
+
+        }
+        // Create a clas. In that class, creat a void method that takes two integers
+        // as parameters. Have the method do a math operation on the first integer
+        // and display the second integer to the screen.
+        public void takeInNums(int numOne, int numTwo)
+        {
+            firstInteger = numOne + 10;
+            secondInteger = numTwo;
+            Console.WriteLine(secondInteger);
+        }
+
+        // Declaration of state of this mathOps class.
+        int firstInteger;
+        int secondInteger;
+    }
+}
---
+++ ClassAndMethodExercise2/ClassAndMethodExercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the class
+            MathOps mathOpsOne = new MathOps();
+            // Call the method in the class, passing in two numbers.
+            mathOpsOne.takeInNums(5, 20);
+            //Call the method in the class, specifying thre parameters by name.
+            mathOpsOne.takeInNums(numOne: 5, numTwo: 35);
+
+
+            Console.ReadLine();
+            
+        }
+    }
+}
