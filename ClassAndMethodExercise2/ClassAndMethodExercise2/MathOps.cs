2752ca53e4d29637400af09c063951e2e1e01457
Alex Hampton <ahampton5158@gmail.com>
2019-09-12 07:58:47 +0900

    Add comment

--- ClassAndMethodExercise2/ClassAndMethodExercise2/MathOps.cs
+++ ClassAndMethodExercise2/ClassAndMethodExercise2/MathOps.cs
@@ -12,9 +12,9 @@ namespace ClassAndMethodExercise2
         {
 
         }
-        // Create a clas. In that class, creat a void method that takes two integers
-        // as parameters. Have the method do a math operation on the first integer
-        // and display the second integer to the screen.
+        // Create a class. In that class, create a void method that takes two 
+        // integers as parameters. Have the method do a math operation on the 
+        // first integer and display the second integer to the screen.
         public void takeInNums(int numOne, int numTwo)
         {
             firstInteger = numOne + 10;
