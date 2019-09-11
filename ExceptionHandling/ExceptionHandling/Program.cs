b0b325e3db0ac61d67276de5b9deab4076929342
Alex Hampton <ahampton5158@gmail.com>
2019-09-11 11:09:56 +0900

    Add Exception Handling Tuturial

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Pick a number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second number: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividing the two...");
+            int number3 = number1 / number2;
+            Console.WriteLine(number1 + " divided by " + number2 + " equals " + number3);
+            Console.ReadLine();
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please type a whole number.");
+            return;
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by zero.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally // The finally will run no matter what. Whether the catches occur or not.
+        {
+            Console.ReadLine();
+        }
+
+        
+    }
+}
