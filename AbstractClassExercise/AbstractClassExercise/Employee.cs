00cf4df68fe35d06973229bf05522a62e1f0b9ee
Alex Hampton <ahampton5158@gmail.com>
2019-09-13 08:17:25 +0900

    Update Employee class to Generic.

--- AbstractClassExercise/AbstractClassExercise/Employee.cs
+++ AbstractClassExercise/AbstractClassExercise/Employee.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace AbstractClassExercise
 {
-    class Employee : Person, IQuittable
+    class Employee<T> : Person, IQuittable // Make Employee class Generic by adding <T> to it. 
     {
+        public Employee()
+        {
+
+        }
         public override void SayName()
         {
             Console.WriteLine("Name: {0} {1}", FirstName, LastName);
@@ -18,15 +22,34 @@ namespace AbstractClassExercise
             Console.WriteLine("{0} {1} has left the building.", FirstName, LastName);
         }
 
-        public static bool operator== (Employee e1, Employee e2) //Overloading operator
+        public static bool operator== (Employee<T> e1, Employee<T> e2) //Overloading operator
         {
             return e1.Id == e2.Id;
         }
-        public static bool operator!= (Employee e1, Employee e2) // Overloading operator
+        public static bool operator!= (Employee<T> e1, Employee<T> e2) // Overloading operator
         {
             return e1.Id != e2.Id;
         }
 
+        public override bool Equals(object obj) //Overrides added due to warning messages.
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
         public int Id { get; set; }
+        public List<T> Things { get; set; } // Things Generic List.
+
+        public void PrintThings()
+        {
+            foreach (T thing in Things)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+
     }
 }
--- AbstractClassExercise/AbstractClassExercise/Program.cs
+++ AbstractClassExercise/AbstractClassExercise/Program.cs
@@ -10,18 +10,27 @@ namespace AbstractClassExercise
     {
         static void Main(string[] args)
         {
-            Employee employee1 = new Employee() { FirstName = "Sample", LastName = "Student", Id = 1 };
-            employee1.SayName();
-            employee1.Quit();
+            //Employee employee1 = new Employee() { FirstName = "Sample", LastName = "Student", Id = 1 };
+            //employee1.SayName();
+            //employee1.Quit();
 
-            IQuittable employee2 = new Employee() { FirstName = "Example", LastName = "Employee", Id = 2};
-            Employee employee3 = new Employee() { FirstName = "Emilio", LastName = "Estevez", Id = 1 };
-            employee2.Quit();
-            Console.WriteLine();
+            //IQuittable employee2 = new Employee() { FirstName = "Example", LastName = "Employee", Id = 2 };
+            Employee<string> employee3 = new Employee<string>() { FirstName = "Emilio", LastName = "Estevez", Id = 1 };
+            //employee2.Quit();
+            //Console.WriteLine();
 
-            Console.WriteLine("employee1 Id is: {0}, employee3 Id is: {1}. Are they the same person?", employee1.Id, employee3.Id);
+            //Console.WriteLine("employee1 Id is: {0}, employee3 Id is: {1}. Are they the same person?", employee1.Id, employee3.Id);
 
-            Console.WriteLine(employee1 == employee3);
+            //Console.WriteLine(employee1 == employee3);
+
+            Employee<string> employee4 = new Employee<string>() { FirstName = "Another", LastName = "Employee", Id = 4, Things = { "pen", "notepad", "glasses" } };
+            Employee<int> employee5 = new Employee<int>() { FirstName = "Bender", LastName = "Rodriguez", Id = 5 };
+
+            Console.WriteLine(employee5.FirstName);
+            
+            //employee4.PrintThings();
+            //employee5.PrintThings();
+
 
             Console.ReadLine();
         }
