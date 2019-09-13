f12d2bc87027d194ae5a9b12b08344c3f7a57d14
Alex Hampton <ahampton5158@gmail.com>
2019-09-13 09:29:27 +0900

    Update generic class. Instantiate List Objects

--- AbstractClassExercise/AbstractClassExercise/Program.cs
+++ AbstractClassExercise/AbstractClassExercise/Program.cs
@@ -15,7 +15,7 @@ namespace AbstractClassExercise
             //employee1.Quit();
 
             //IQuittable employee2 = new Employee() { FirstName = "Example", LastName = "Employee", Id = 2 };
-            Employee<string> employee3 = new Employee<string>() { FirstName = "Emilio", LastName = "Estevez", Id = 1 };
+            //Employee<string> employee3 = new Employee<string>() { FirstName = "Emilio", LastName = "Estevez", Id = 1 };
             //employee2.Quit();
             //Console.WriteLine();
 
@@ -23,13 +23,16 @@ namespace AbstractClassExercise
 
             //Console.WriteLine(employee1 == employee3);
 
-            Employee<string> employee4 = new Employee<string>() { FirstName = "Another", LastName = "Employee", Id = 4, Things = { "pen", "notepad", "glasses" } };
-            Employee<int> employee5 = new Employee<int>() { FirstName = "Bender", LastName = "Rodriguez", Id = 5 };
+            Employee<string> employee4 = new Employee<string>() { FirstName = "Another", LastName = "Employee", Id = 4,
+                Things = new List<string>() { "pen", "notepad", "glasses" } };
+            Employee<int> employee5 = new Employee<int>() { FirstName = "Bender", LastName = "Rodriguez", Id = 5,
+                Things = new List<int>() { 0, 1 } };
 
-            Console.WriteLine(employee5.FirstName);
-            
-            //employee4.PrintThings();
-            //employee5.PrintThings();
+            Console.WriteLine("employee4's things: ");
+            employee4.PrintThings();
+            Console.WriteLine();
+            Console.WriteLine("employee5's things: ");
+            employee5.PrintThings();
 
 
             Console.ReadLine();
