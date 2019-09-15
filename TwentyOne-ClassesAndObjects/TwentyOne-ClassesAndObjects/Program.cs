852d6581ef919b10609197de96407a9e1d9aa124
Alex Hampton <ahampton5158@gmail.com>
2019-09-15 18:24:12 +0900

    Practice lambda expressions

--- TwentyOne-ClassesAndObjects/TwentyOne-ClassesAndObjects/Program.cs
+++ TwentyOne-ClassesAndObjects/TwentyOne-ClassesAndObjects/Program.cs
@@ -10,13 +10,8 @@ namespace TwentyOne_ClassesAndObjects
     {
         static void Main(string[] args)
         {
-            Card card1 = new Card();
-            Card card2 = card1;
-            card1.Face = Face.Eight;
-            card2.Face = Face.King;
-            Console.WriteLine(card1.Face);
-
-            //Deck deck = new Deck();
+            Deck deck = new Deck();
+            
             //deck.Shuffle(3);
 
             //foreach (Card card in deck.Cards)
@@ -24,10 +19,30 @@ namespace TwentyOne_ClassesAndObjects
             //    Console.WriteLine(card.Face + " of " + card.Suit);
             //}
             //Console.WriteLine("Total card count: " + deck.Cards.Count);
+
+
+
             Console.Read();
 
 
 
+            /* LAMBDA EXPRESSION PRACTICE */
+
+            //int count = deck.Cards.Count(x => x.Face == Face.Ace); // .Count Lambda function.
+            //Console.WriteLine(count);
+            //List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList();
+            //List<int> numberList = new List<int> { 1, 2, 3, 4365, 234, 34 };
+            //int sum = numberList.Sum();
+            //int y = numberList.Sum(x => x += 5);
+            //int max = numberList.Max();
+            //int min = numberList.Min();
+            //int chain = numberList.Where(x => x > 20).Sum();
+
+            //foreach (Card card in newList)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+                                          
             //Game game = new TwentyOneGame();
             //game.Players = new List<Player>();
             //    Player player = new Player() { Name = "Alex" };
