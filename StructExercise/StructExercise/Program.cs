7e430ccf9d4d6d9f7574dd16d4e2c2e25a56f0fe
Alex Hampton <ahampton5158@gmail.com>
2019-09-15 13:04:29 +0900

    Add Struct Exercise

---
+++ StructExercise/StructExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number numOne = new Number() { Amount = 2.345m};
+            Console.WriteLine(numOne.Amount);
+
+            Console.Read();
+        }
+    }
+}
