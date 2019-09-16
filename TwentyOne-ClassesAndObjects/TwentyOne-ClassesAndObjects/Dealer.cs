67ef242f4d1e77652133825d4cc63dcfd1257201
Alex Hampton <ahampton5158@gmail.com>
2019-09-16 14:41:39 +0900

    Practice using DateTime

--- TwentyOne-ClassesAndObjects/TwentyOne-ClassesAndObjects/Dealer.cs
+++ TwentyOne-ClassesAndObjects/TwentyOne-ClassesAndObjects/Dealer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TwentyOne_ClassesAndObjects
 {
@@ -15,7 +16,13 @@ namespace TwentyOne_ClassesAndObjects
         public void Deal(List<Card> Hand)
         {
             Hand.Add(Deck.Cards.First());
-            Console.WriteLine(Deck.Cards.First().ToString() + "\n");
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\alext\OneDrive\Documents\GitHub\The-Tech-Academy-Basic-C-Sharp-Projects\Logs\log.txt", true))
+            {
+                file.WriteLine(DateTime.Now);
+                file.WriteLine(card);
+            }
             Deck.Cards.RemoveAt(0);
         }
     }
