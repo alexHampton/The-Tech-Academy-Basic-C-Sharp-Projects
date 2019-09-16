897de0962e90527a51fc85709589ec8fe8ceed7b
Alex Hampton <ahampton5158@gmail.com>
2019-09-16 14:40:50 +0900

    Add DateTime Exercise

---
+++ DateTimeExercise/DateTimeExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please enter a number: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            TimeSpan tshours = new TimeSpan(hours, 0, 0);
+            DateTime future = new DateTime();
+            future = DateTime.Now + tshours;
+            Console.WriteLine("The time in {0} hours will be {1}", hours, future);
+
+            Console.Read();
+        }
+    }
+}
