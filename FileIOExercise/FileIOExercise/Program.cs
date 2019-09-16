8132791352b2d96c9cba059c0aac44a0e6177cb3
Alex Hampton <ahampton5158@gmail.com>
2019-09-16 13:04:26 +0900

    Add File IO Exercise

---
+++ FileIOExercise/FileIOExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number: ");
+            string userInput = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter(@"C:\Users\alext\OneDrive\Documents\GitHub\The-Tech-Academy-Basic-C-Sharp-Projects\Logs\log2.txt", true))
+            {
+                file.WriteLine(userInput);
+            }
+            Console.WriteLine(File.ReadAllText(@"C:\Users\alext\OneDrive\Documents\GitHub\The-Tech-Academy-Basic-C-Sharp-Projects\Logs\log2.txt"));
+            Console.Read();
+        }
+    }
+}
