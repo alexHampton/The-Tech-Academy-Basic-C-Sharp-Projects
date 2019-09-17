128a2960ebe515f4ab7d72837ccb834df87bc2db
Alex Hampton <ahampton5158@gmail.com>
2019-09-17 18:01:46 +0900

    Add ExceptionHandlingExcercise2

---
+++ ExceptionHandlingExercise2/ExceptionHandlingExercise2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello.");
+
+                int days = 0;
+                Console.WriteLine("How old are you?");
+                days = Convert.ToInt32(Console.ReadLine()) * 365;
+                if (days <= 0)
+                {
+                    throw new IntegerFormatException();
+                }
+
+                TimeSpan tsdays = new TimeSpan();
+                tsdays = TimeSpan.FromDays(days);
+                DateTime birthyear = new DateTime();
+                birthyear = DateTime.Now - tsdays;
+                Console.WriteLine("You were born in {0}", birthyear.Year);
+
+                Console.ReadLine();
+            }
+            catch (IntegerFormatException ex)
+            {
+                Console.WriteLine("Sorry, you must currently be alive to use this program.");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
